b2627d6a8e0142221160344b5ea4863ba42ca652
Manasgupta840 <manasgupta@Mac.lan>
2025-12-25 16:16:12 +0530

    fix: branch structure

--- src/submit.js
+++ src/submit.js
@@ -1,11 +1,17 @@
 // submit.js
 
+import { nodeServices } from "./services/nodesServices";
 import { useStore } from "./store";
 
 export const SubmitButton = () => {
   const { nodes, edges } = useStore();
 
   const onSubmit = () => {
+    const body = {
+      nodes,
+      edges,
+    };
+    nodeServices.addPipeline(body);
     console.log(nodes, edges);
   };
 
